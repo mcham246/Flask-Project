d1e88adb158d37494c12a2633033996296a58fcf
Matarr Cham <mcham8@outlook.com>
2023-03-28 10:47:50 -0400

    table creation

--- flask_app/main/routes.py
+++ flask_app/main/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
-
+from ..data_manager.data_scraper import DataScraper
 main = Blueprint("main", __name__)
 
 @main.route('/')
 def index():
+    object = DataScraper()
+    df = object.getOffensiveLeadersStats()
     return render_template('index.html')
